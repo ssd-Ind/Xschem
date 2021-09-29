a3079827bcbabe15b7706bd96ccbc865afd86970
ssd-Ind <91599323+ssd-Ind@users.noreply.github.com>
2021-09-29 15:05:27 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo apt-get update -y
 sudo apt install libx11-dev libxrender1 libxrender-dev libxcb1 tcl8.6 \
                  libx11-xcb-dev libcairo2 libcairo2-dev tcl8.6-dev tk8.6 \
                  tk8.6-dev flex bison libxpm4 libxpm-dev gawk mawk
+cd
+git clone https://github.com/StefanSchippers/xschem.git
