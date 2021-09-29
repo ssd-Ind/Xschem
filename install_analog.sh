8d9cc7a2316195620c5439d1cc3ce9ca336d096c
ssd-Ind <91599323+ssd-Ind@users.noreply.github.com>
2021-09-29 17:59:37 +0530

    Update and rename install.sh to install_analog.sh

---
+++ install_analog.sh
@@ -0,0 +1,32 @@
+sudo apt update
+sudo apt install git
+sudo apt-get update -y
+sudo apt install libx11-dev libxrender1 libxrender-dev libxcb1 tcl8.6 \
+                 libx11-xcb-dev libcairo2 libcairo2-dev tcl8.6-dev tk8.6 \
+                 tk8.6-dev flex bison libxpm4 libxpm-dev gawk mawk
+cd
+git clone https://github.com/StefanSchippers/xschem.git
+cd xschem
+./configure
+make -j4
+sudo make install
+xschem &
+cd
+cd .xschem
+mkdir xschem_library
+cd xschem_library
+git clone https://github.com/StefanSchippers/xschem_sky130.git xschem_sky130
+xschem &
+cd
+git clone https://github.com/google/skywater-pdk
+cd skywater-pdk
+git submodule init libraries/sky130_fd_io/latest
+git submodule init libraries/sky130_fd_pr/latest
+git submodule init libraries/sky130_fd_sc_hd/latest
+git submodule init libraries/sky130_fd_sc_hvl/latest
+git submodule init libraries/sky130_fd_sc_hdll/latest
+git submodule init libraries/sky130_fd_sc_hs/latest
+git submodule init libraries/sky130_fd_sc_ms/latest
+git submodule init libraries/sky130_fd_sc_ls/latest
+git submodule init libraries/sky130_fd_sc_lp/latest
+git submodule update
