4191b041e39e62eae9fe76805e62663c9f4fed5a
ssd-Ind <91599323+ssd-Ind@users.noreply.github.com>
2021-10-01 21:14:31 +0530

    Update install_analog.sh

--- install_analog.sh
+++ install_analog.sh
@@ -5,7 +5,7 @@ sudo apt install libx11-dev libxrender1 libxrender-dev libxcb1 tcl8.6 \
                  libx11-xcb-dev libcairo2 libcairo2-dev tcl8.6-dev tk8.6 \
                  tk8.6-dev flex bison libxpm4 libxpm-dev gawk mawk automake \
                  libtool build-essential gperf libxml2 libxml2-dev libxml-libxml-perl \
-                 libgd-perl libxaw7-dev libreadline6-dev vim-gtk3 --assume-yes
+                 libgd-perl libxaw7-dev libreadline6-dev vim-gtk3 xterm --assume-yes
 cd
 git clone https://github.com/StefanSchippers/xschem.git
 cd xschem
