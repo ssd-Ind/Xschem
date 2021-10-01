0b2959b747de12a25c67e7cad29b48238274c1e0
ssd-Ind <91599323+ssd-Ind@users.noreply.github.com>
2021-10-01 22:56:04 +0530

    Update install_analog.sh

--- install_analog.sh
+++ install_analog.sh
@@ -61,6 +61,10 @@ cd
 # Press i
 # :wq
 # cd
+git clone https://github.com/ssd-Ind/Xschem.git hspiceinit
+cd hspiceinit
+cp .spiceinit ~/.xschem/simulations
+cd
 cd .xschem/xschem_library/xschem_sky130
 xschem &
 
